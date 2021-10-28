c625cd7c178a0588dfa659b2c75bd0737cf95b27
Murphy Studebaker <murphy.stude@gmail.com>
2021-10-28 16:03:20 -0700

    header challenge completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,16 +29,33 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopIcons>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopIcons>
         <Logo />
+        <AuthActions>
+          <Button>Subscribe</Button>
+          <button className="subscriber">Already a subscriber?</button>
+        </AuthActions>
       </MainHeader>
     </header>
   );
 };
 
+
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+      display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +76,42 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopIcons = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+     display: flex;
+  }
+`
+
+const AuthActions = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+     display: flex;
+     flex-direction: column;
+     gap: 8px;
+     justify-content: center;
+     align-items: center; 
+     padding-top: 8px;
+     .subscriber {
+       text-decoration: underline;
+       font-style: italic;
+       text-align: center;
+     }
+  }
+`
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: space-between;
+  }
 `;
 
 export default Header;
